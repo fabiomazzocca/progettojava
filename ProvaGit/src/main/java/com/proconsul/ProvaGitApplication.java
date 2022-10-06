d4687432cadc7d82bbf74329f5b42b4ade4eddc9
fabio <fabio@LAPTOP-GJRKGEOD.homenet.telecomitalia.it>
2022-10-06 10:42:34 +0200

    prova

---
+++ ProvaGit/src/main/java/com/proconsul/ProvaGitApplication.java
@@ -0,0 +1,13 @@
+package com.proconsul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaGitApplication.class, args);
+	}
+
+}
---
+++ ProvaGit/src/test/java/com/proconsul/ProvaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proconsul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
